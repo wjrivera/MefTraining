377d4ce08f7fa89d05e92823ca30e2661ec2e00b
William Rivera <wjrivera@eagle.fgcu.edu>
2020-05-30 01:29:33 -0400

    Adding files

---
+++ MefTraining/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace MefTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Part 1
+            var partOne = new PartOne();
+            Console.WriteLine(partOne.Init());
+
+            Console.ReadLine();
+        }
+
+        
+    }
+    public class PartOne
+    {
+        //ExportedMessage(string) => Message(string)
+        [Export] public string ExportedMessage = "Exported Message";
+        [Import] public string Message;
+
+        public string Init()
+        {
+            var section = "Part - 1: ";
+            var catalogTypeOf = new AssemblyCatalog(typeof(Program).Assembly);
+            //var catalog = new AssemblyCatalog(System.Reflection.Assembly.GetExecutingAssembly());
+            var container = new CompositionContainer(catalogTypeOf);
+            var p = new PartOne();
+            container.ComposeParts(p);
+            return $"{section}{p.Message}";
+        }
+    }
+}
