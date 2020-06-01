70679ad85c138ee5956f37b84a88587ad4a439b6
William Rivera <wjrivera@eagle.fgcu.edu>
2020-06-01 10:25:35 -0400

    Minor stuff

---
+++ ExtensionExports/ExtensionExportingClass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel.Composition;
+
+namespace ExtensionExports
+{
+    public interface IExportMetadata
+    {
+        public string Uppercase { get; }
+    }
+    
+    public interface IExports
+    {
+        public string Message { get; set; }
+    }
+
+    [InheritedExport]
+    public interface IInheritedExports
+    {
+        public string Message { get; set; }
+    }
+    
+    [Export(typeof(IExports))]
+    [ExportMetadata("Uppercase", "X")]
+    public class ExtensionExportingClass : IExports
+    {
+        public string Message { get; set; }
+
+        public ExtensionExportingClass()
+        {
+            Message = "Inner Message 1";
+        }
+    }
+    
+    [Export(typeof(IExports))]
+    [ExportMetadata("Uppercase", "Y")]
+    public class ExtensionExportingClassTwo : IExports
+    {
+        public string Message { get; set; }
+
+        public ExtensionExportingClassTwo()
+        {
+            Message = "Inner Message 2";
+        }
+    }
+    
+    //[ExportMetadata("Uppercase", "1")]
+    public class InheritedExtensionExportingClassOne : IInheritedExports
+    {
+        public string Message { get; set; }
+
+        public InheritedExtensionExportingClassOne()
+        {
+            Message = "Inherited Inner Message 1";
+        }
+    }
+    
+    //[ExportMetadata("Uppercase", "2")]
+    public class InheritedExtensionExportingClassTwo : IInheritedExports
+    {
+        public string Message { get; set; }
+
+        public InheritedExtensionExportingClassTwo()
+        {
+            Message = "Inherited Inner Message 2";
+        }
+    }
+}
